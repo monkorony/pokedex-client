ad3671e42e61ceb0d06a6c3495c269d53518d2a2
Sirin Srikier <sirinsrikier@Sirins-MacBook-Pro.local>
2019-09-18 19:46:15 -0700

    update build

---
+++ build/precache-manifest.a3176f67eb16f6f3aeb40818682a0b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5418a291a737fae642e61dad35b284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f407d4931c0ecf7bcdeb",
+    "url": "/static/css/main.93c32a82.chunk.css"
+  },
+  {
+    "revision": "9cff94d87e3c4d4f4e95",
+    "url": "/static/js/2.ac89d6a5.chunk.js"
+  },
+  {
+    "revision": "f407d4931c0ecf7bcdeb",
+    "url": "/static/js/main.99a86451.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad568a2cacdfae057e32a1fc4a7589e3",
+    "url": "/static/media/charmeleonLoad.ad568a2c.gif"
+  },
+  {
+    "revision": "4d4737a8e266be772dbff2603c1f3466",
+    "url": "/static/media/pokemonGroup.4d4737a8.png"
+  }
+]);
